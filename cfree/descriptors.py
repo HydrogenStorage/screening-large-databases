54a5164c030f50a2733d987e78ab724aeb47ca22
Logan Ward <ward.logan.t@gmail.com>
2023-02-21 15:35:30 -0500

    Add wt%h2 computation

---
+++ cfree/descriptors.py
@@ -0,0 +1,42 @@
+"""Tools for computing descriptors related to hydrogen storage capability"""
+from typing import Optional
+
+from rdkit.Chem import Descriptors
+from rdkit import Chem
+
+
+def saturate_molecule(molecule_smiles: str) -> str:
+    """Generate a fully-saturated version of a molecule
+
+    Args:
+        molecule_smiles: SMILES string of molecule to be saturated
+    Returns:
+        SMILES string of saturated molecule
+    """
+    # Code originally by Hassan Harb
+    new_smiles = ''
+    for i in molecule_smiles:
+        if i == '=' or i == '#':  # Skip double and triple bonds
+            continue
+        elif i in {'c', 'n', 'o', 's'}:
+            i = i.upper()
+            new_smiles = new_smiles + i
+        else:
+            new_smiles = new_smiles + i
+    return new_smiles
+
+
+def compute_wth2(smiles_dehydrogenated: str, smiles_hydrogenated: Optional[str] = None) -> float:
+    """Compute the wt% of hydrogen that is stored by a molecule
+
+    Args:
+        smiles_dehydrogenated: Dehydrogenated form of the molecule
+        smiles_hydrogenated: Hydrogenated form. If `None`, we will compute the fully-hydrogenated form
+    Returns:
+        wt% of hydrogen stored by molecule
+    """
+    if smiles_hydrogenated is None:
+        smiles_hydrogenated = saturate_molecule(smiles_dehydrogenated)
+    mw_hydrogenated = Chem.Descriptors.ExactMolWt(Chem.MolFromSmiles(smiles_hydrogenated))
+    mw_dehydrogenated = Chem.Descriptors.ExactMolWt(Chem.MolFromSmiles(smiles_dehydrogenated))
+    return ((mw_hydrogenated - mw_dehydrogenated) / mw_hydrogenated) * 100
---
+++ cfree/tests/test_descriptors.py
@@ -0,0 +1,13 @@
+from math import isclose
+
+from cfree.descriptors import compute_wth2, saturate_molecule
+
+
+def test_hydrogenate():
+    assert saturate_molecule('C=C') == 'CC'
+    assert saturate_molecule('c1cnccc1') == 'C1CNCCC1'
+
+
+def test_wth2():
+    assert isclose(compute_wth2('C=C=C'), 9.14, abs_tol=1e-2)
+    assert isclose(compute_wth2('C=C=C', 'C=CC'), 4.79, abs_tol=1e-2)
